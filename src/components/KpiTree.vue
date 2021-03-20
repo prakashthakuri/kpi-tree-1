bae577b584ef0961927a704344f88b0e67f5ae4c
Prakash Shahi Thakuri <prakashthakuri1994@gmail.com>
2021-03-19 20:10:00 -0500

    Vue KPI Tree v1.0 - Fix Patches

--- src/components/KpiTree.vue
+++ src/components/KpiTree.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-
     <div class="tree-container" ref="container">
-      <vue-tree 
-        ref="tree"
-          :style="{width: treeW + 'px', height: treeH + 'px'}"
-          :dataset = "tree"
-          :config= "treeConfig"
-          linkStyle = "straight">
-
-          <template v-slot:node="{ node, collapsed, index }">
+        <vue-tree 
+            ref="tree"
+            :style="{width: treeW + 'px', height: treeH + 'px'}"
+            :dataset="tree" 
+            :config="treeConfig"
+            linkStyle="straight">
+            <template v-slot:node="{ node, collapsed, index }">
                 <div class="node-item">
                     <div style="width:100%">
                         <div class="node-title">{{node.name}}</div>                        
@@ -38,16 +36,11 @@
             <div class="btn-toolbar" @click="zoomOut"><v-icon name="minus"></v-icon></div>
         </div>
     </div>
-
-
-  
   </div>
 </template>
 
 <script>
-
- import VueTree from './VueTree'
-
+import VueTree from './VueTree'
 
 export default {
   name: 'KpiTree',
@@ -57,78 +50,81 @@ export default {
   components: {
     VueTree
   },
-
+  
   data: function() {
-      return {
-          treeW: 1200,
-          treeH:800,
-          treeConfig:{
-            nodeWidth:240,
-            nodeHeight:100,
-            levelHeight: 100
-          },
-          // Static JSON data
-          tree: {
-            name: "EDITDA", 
-            value: {amount: "123", color: "green"}, 
-            delta: {amount:"12%", color: "green"}, 
-            children: [
-              {
-                name: "Customer Profitability",
-                value: {amount: "123", color: "green"}, 
-                delta: {amount: "12%", color: "red"}, 
-                children: [
-                  {
-                    name: "MRR", 
-                    value: {amount: "123", color: "orange"}, 
-                    delta: {amount: "12%", color: "green"}, 
-                    children: [
-                      {
-                        name: "ACV",
-                        value: {amount: "123", color: "orange"}, 
-                        delta: {amount: "12%", color: "red"}, 
-                      }
-                    ]
-                  },
-                  {
-                    name: "Total CAC",
-                    value: {amount: "123", color: "green"}, 
-                    delta: {amount: "12%", color: "green"}, 
-                    children: [
-                      {
-                        name: "Customer Success",
-                        value: {amount: "123", color: "red"}, 
-                        delta: {amount: "12%", color: "green"},
-                      },
-                      {
-                        name: "Sales & Marketing",
-                        value: {amount: "123", color: "green"}, 
-                        delta: {amount: "12%", color: "green"},
-                      }
-                    ],
-                  }
-                ]
-              },
-              {
-                name: "NON-CAC related",
-                value: {amount: "123", color: "green"}, 
-                delta: {amount:"12%", color: "orange"},
-                children: [
-                  {
-                    name: "R&D (excl. Support)",
-                    value: {amount: "123", color: "green"}, 
-                    delta: {amount: "12%", color: "green"},                      
-                  },
-                  {
-                    name: "G&A",
-                    value: {amount: "123", color: "green"}, 
-                    delta: {amount: "12%", color: "green"},  
-                  }
-                ]                 
-              }
-            ]
-          }
-      }
+    return {
+        treeW: 1200,
+        treeH: 800,
+        treeConfig: { 
+            nodeWidth: 240,
+            nodeHeight: 100, 
+            levelHeight: 100 
+        },
+        tree: {
+          name: "EDITDA", 
+          value: {amount: "123", color: "green"}, 
+          delta: {amount:"12%", color: "green"}, 
+          children: [
+            {
+              name: "Customer Profitability",
+              value: {amount: "123", color: "green"}, 
+              delta: {amount: "12%", color: "red"}, 
+              children: [
+                {
+                  name: "MRR", 
+                  value: {amount: "123", color: "orange"}, 
+                  delta: {amount: "12%", color: "green"}, 
+                  children: [
+                    {
+                      name: "ACV",
+                      value: {amount: "123", color: "orange"}, 
+                      delta: {amount: "12%", color: "red"}, 
+                    }
+                  ]
+                },
+                {
+                  name: "Total CAC",
+                  value: {amount: "123", color: "green"}, 
+                  delta: {amount: "12%", color: "green"}, 
+                  children: [
+                    {
+                      name: "Customer Success",
+                      value: {amount: "123", color: "red"}, 
+                      delta: {amount: "12%", color: "green"},
+                    },
+                    {
+                      name: "Sales & Marketing",
+                      value: {amount: "123", color: "green"}, 
+                      delta: {amount: "12%", color: "green"},
+                    }
+                  ],
+                }
+              ]
+            },{ 
+              name: "EDITDA", 
+              value: {amount: "123", color: "green"}, 
+              delta: {amount:"12%", color: "green"}, 
+            },
+            {
+              name: "NON-CAC related",
+              value: {amount: "123", color: "green"}, 
+              delta: {amount:"12%", color: "orange"},
+              children: [
+                {
+                  name: "R&D (excl. Support)",
+                  value: {amount: "123", color: "green"}, 
+                  delta: {amount: "12%", color: "green"},                      
+                },
+                {
+                  name: "G&A",
+                  value: {amount: "123", color: "green"}, 
+                  delta: {amount: "12%", color: "green"},  
+                }
+              ]                 
+            }
+          ]
+        }
+    }
   },
   mounted() {
     this.resizeHandler()
@@ -146,7 +142,6 @@ export default {
 
       this.$refs.tree.setTransform(this.treeW, this.treeH)
     },
-    
     onClickNode (index) {
       this.$refs.tree.onClickNode(index)
     },
@@ -158,6 +153,7 @@ export default {
       } else if( icon === "orange" ) {
         return "caret-right"
       }
+
       return ""
     },
     zoomIn () {
@@ -169,13 +165,11 @@ export default {
     zoomReset () {
       this.$refs.tree.restoreScale()
     }
-
+  }
 }
-}
-
 </script>
 
- <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -191,7 +185,6 @@ li {
 a {
   color: #42b983;
 }
-
 
 .tree-container {
   width: 100%;
@@ -264,4 +257,5 @@ a {
 .orange {  
   color: #F0AB00;
 }
+
 </style>
--- src/components/VueTree.vue
+++ src/components/VueTree.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tree-container" ref="container">
+  <div class="tree-container" ref="container">
     <svg class="svg vue-tree" ref="svg" :style="initialTransformStyle"></svg>
 
     <div
@@ -44,12 +44,13 @@ const MATCH_TRANSLATE_REGEX = /translate\((-?\d+)px, ?(-?\d+)px\)/i
 const MATCH_SCALE_REGEX = /scale\((\S*)\)/i
 
 const LinkStyle = {
-    CURVE: 'curve',
-    STRAIGHT: 'straight'
+  CURVE: 'curve',
+  STRAIGHT: 'straight'
 }
+
 const DIRECTION = {
-    VERTICL: 'vertical',
-    HORIZONTAL: 'horizontal'
+  VERTICAL: 'vertical',
+  HORIZONTAL: 'horizontal'
 }
 
 const DEFAULT_NODE_WIDTH = 100
@@ -59,248 +60,244 @@ const DEFAULT_LEVEL_HEIGHT = 200
 const ANIMATION_DURATION = 800
 
 function uuid() {
-    const s= []
-    const hexDigits = '0123456789abcdef'
-
-    for (let i = 0; i<36; i++) {
-        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
-
-    }
-    s[14] ='4'
-    s[19] = hexDigits.substr((s[19] &0x3) | 0x8, 1)
-    s[8] = s[13] = s[18] = s[23] = '-'
-    return s.join('')
+  const s = []
+  const hexDigits = '0123456789abcdef'
+  for (let i = 0; i < 36; i++) {
+    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
+  }
+  s[14] = '4'
+  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1)
+  s[8] = s[13] = s[18] = s[23] = '-'
+  return s.join('')
 }
 
-function rotatePoint({x,y}) {
-    return {
-        x:y,
-        y:x
-    }
+function rotatePoint({ x, y }) {
+  return {
+    x: y,
+    y: x
+  }
 }
 
 export default {
-    name: 'vue-tree',
-    props: {
-        config: {
-            type: Object,
-            default: () => {
-                return {
-                    nodeWidth: DEFAULT_NODE_WIDTH,
-                    nodeHeight: DEFAULT_NODE_HEIGHT,
-                    levelHeight: DEFAULT_LEVEL_HEIGHT
-                }
-            }
-        },
-        LinkStyle: {
-            type:String,
-            default:LinkStyle.CURVE
-        },
-        direction: {
-            type: String,
-            default: DIRECTION.VERTICL
-        },
-        dataset: {
-            type: Object,
-            required: true
-        }
-    },
-
-    data() {
+  name: 'vue-tree',
+  props: {
+    config: {
+      type: Object,
+      default: () => {
         return {
-            d3,
-            colors: '568FE1',
-            nodeDataList: [],
-            linkDataList: [],
-            initTransformX: 0,
-            initTransformY: 0,
-            DIRECTION,
-            currentScale: 1
+          nodeWidth: DEFAULT_NODE_WIDTH,
+          nodeHeight: DEFAULT_NODE_HEIGHT,
+          levelHeight: DEFAULT_LEVEL_HEIGHT
         }
-        
+      }
     },
-    computed: {
-        initialTransformStyle() {
-            return {
-                transform: `scale(1) translate(${this.initTransformX}px, ${this.initTransformY}px`,
-                transformOrigin: 'center'
-            }
+    linkStyle: {
+      type: String,
+      default: LinkStyle.CURVE
+    },
+    direction: {
+      type: String,
+      default: DIRECTION.VERTICAL
+    },
+    
+    dataset: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      d3,
+      colors: '568FE1',
+      nodeDataList: [],
+      linkDataList: [],
+      initTransformX: 0,
+      initTransformY: 0,
+      DIRECTION,
+      currentScale: 1
+    }
+  },
+  computed: {
+    initialTransformStyle() {
+      return {
+        transform: `scale(1) translate(${this.initTransformX}px, ${this.initTransformY}px)`,
+        transformOrigin: 'center'
+      }
+    }
+  },
+  created() {
+    this.addUniqueKey(this.dataset)
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.draw()
+      this.enableDrag()
+      this.initTransform()
+    },
+    zoomIn() {
+      const originTransformStr = this.$refs.domContainer.style.transform
+      
+      let targetScale = 1 * 1.2
+      const scaleMatchResult = originTransformStr.match(MATCH_SCALE_REGEX)
+      if (scaleMatchResult && scaleMatchResult.length > 0) {
+        const originScale = parseFloat(scaleMatchResult[1])
+        targetScale *= originScale
+      }
+      this.setScale(targetScale)
+    },
+    zoomOut() {
+      const originTransformStr = this.$refs.domContainer.style.transform
+      
+      let targetScale = 1 / 1.2
+      const scaleMatchResult = originTransformStr.match(MATCH_SCALE_REGEX)
+      if (scaleMatchResult && scaleMatchResult.length > 0) {
+        const originScale = parseFloat(scaleMatchResult[1])
+        targetScale = originScale / 1.2
+      }
+      this.setScale(targetScale)
+    },
+    restoreScale() {
+      this.setScale(1)
+    },
+    setScale(scaleNum) {
+      if (typeof scaleNum !== 'number') return
+      let pos = this.getTranslate()
+      let translateString = `translate(${pos[0]}px, ${pos[1]}px)`
+      this.$refs.svg.style.transform = `scale(${scaleNum}) ` + translateString
+      this.$refs.domContainer.style.transform =
+        `scale(${scaleNum}) ` + translateString
+      this.currentScale = scaleNum
+    },
+    getTranslate() {
+      let string = this.$refs.svg.style.transform
+      let match = string.match(MATCH_TRANSLATE_REGEX)
+      if (match === null) {
+        return [null, null]
+      }
+      let x = parseInt(match[1])
+      let y = parseInt(match[2])
+      return [x, y]
+    },
+    isVertical() {
+      return this.direction === DIRECTION.VERTICAL
+    },
+    addUniqueKey(rootNode) {
+      const queue = [rootNode]
+      while (queue.length !== 0) {
+        const node = queue.pop()
+        node._key = uuid()
+        if (node.children) {
+          queue.push(...node.children)
         }
+      }
+      return rootNode
     },
-    created() {
-        this.addUniqueKey(this.dataset)
+    initTransform() {
+      const containerWidth = this.$refs.container.offsetWidth
+      const containerHeight = this.$refs.container.offsetHeight
+      if (this.isVertical()) {
+        this.initTransformX = Math.floor(containerWidth / 2)
+        this.initTransformY = Math.floor(this.config.nodeHeight)
+      } else {
+        this.initTransformX = Math.floor(this.config.nodeWidth)
+        this.initTransformY = Math.floor(containerHeight / 2)
+      }
     },
-    mounted() {
-        this.init()
+    setTransform(width, height) {
+      const containerWidth = width
+      const containerHeight = height
+      if (this.isVertical()) {
+        this.initTransformX = Math.floor(containerWidth / 2)
+        this.initTransformY = Math.floor(this.config.nodeHeight)
+      } else {
+        this.initTransformX = Math.floor(this.config.nodeWidth)
+        this.initTransformY = Math.floor(containerHeight / 2)
+      }
     },
-    methods: {
-        init() {
-            this.draw()
-            this.enableDrag()
-            this.initTranform()
-        },
-        zoomIn() {
-            const originTranformStr = this.$refs.domContainer.style.transform
-
-            let targetScale = 1 * 1.2
-            const scaleMatchResult = originTranformStr.match(MATCH_SCALE_REGEX)
-
-            if(scaleMatchResult && scaleMatchResult.length > 0) {
-                const originScale = parseFloat(scaleMatchResult[1])
-                targetScale *= originScale
+    generateLinkPath(d) {
+      const self = this
+      if (this.linkStyle === LinkStyle.CURVE) {
+        const linkPath = this.isVertical()
+          ? d3.linkVertical()
+          : d3.linkHorizontal()
+        linkPath
+          .x(function (d) {
+            return d.x
+          })
+          .y(function (d) {
+            return d.y
+          })
+          .source(function (d) {
+            const sourcePoint = {
+              x: d.source.x,
+              y: d.source.y
             }
-            this.setScale(targetScale)
-        },
-        zoomOut() {
-            const originTranformStr = this.$refs.domContainer.style.transform
-
-            let targetScale = 1 / 1.2
-            const scaleMatchResult = originTranformStr.match(MATCH_SCALE_REGEX)
-
-            if(scaleMatchResult &&scaleMatchResult.length > 0) {
-                const originScale = parseFloat(scaleMatchResult[1])
-                targetScale = originScale / 1.2
+            return self.direction === self.DIRECTION.VERTICAL
+              ? sourcePoint
+              : rotatePoint(sourcePoint)
+          })
+          .target(function (d) {
+            const targetPoint = {
+              x: d.target.x,
+              y: d.target.y
             }
-            this.setScale(targetScale)
-        },
-        restoreScale() {
-            this.setScale(1)
-        },
+            return self.direction === self.DIRECTION.VERTICAL
+              ? targetPoint
+              : rotatePoint(targetPoint)
+          })
+        return linkPath(d)
+      }
+      if (this.linkStyle === LinkStyle.STRAIGHT) {
+        // the link path is: source -> secondPoint -> thirdPoint -> target
+        const linkPath = d3.path()
+        let sourcePoint = { x: d.source.x, y: d.source.y }
+        let targetPoint = { x: d.target.x, y: d.target.y }
+        if (!this.isVertical()) {
+          sourcePoint = rotatePoint(sourcePoint)
+          targetPoint = rotatePoint(targetPoint)
+        }
+        const xOffset = targetPoint.x - sourcePoint.x
+        const yOffset = targetPoint.y - sourcePoint.y
+        const secondPoint = this.isVertical()
+          ? { x: sourcePoint.x, y: sourcePoint.y + yOffset / 2 }
+          : { x: sourcePoint.x + xOffset / 2, y: sourcePoint.y }
+        const thirdPoint = this.isVertical()
+          ? { x: targetPoint.x, y: sourcePoint.y + yOffset / 2 }
+          : { x: sourcePoint.x + xOffset / 2, y: targetPoint.y }
+        linkPath.moveTo(sourcePoint.x, sourcePoint.y)
+        linkPath.lineTo(secondPoint.x, secondPoint.y)
+        linkPath.lineTo(thirdPoint.x, thirdPoint.y)
+        linkPath.lineTo(targetPoint.x, targetPoint.y)
+        return linkPath.toString()
+      }
+    },
+    
+    draw() {
+      const [nodeDataList, linkDataList] = this.buildTree(this.dataset)
+      this.linkDataList = linkDataList
+      this.svg = this.d3.select(this.$refs.svg)
 
-        setScale(scaleNum) {
-            if(typeof scaleNum !== 'number') return
-            let pos = this.getTranslate()
-            let translateString = `translate(${pos[0]}px, ${pos[1]}px`
-            this.$refs.svg.style.transform = `scale(${scaleNum})` + translateString
-            this.$refs.domContainer.style.transform = `scale(${scaleNum} ` + translateString
-            this.currentScale = scaleNum
-        },
-        getTranslate () {
-            let string = this.$refs.svg.style.transform
-            let match = string.match(MATCH_TRANSLATE_REGEX)
-            if(match == null) {
-                return [null, null]
-            }
-            let x = parseInt(match[1])
-            let y = parseInt(match[2])
-            return[x,y]
+      const self = this
+      const links = this.svg.selectAll('.link').data(linkDataList, (d) => {
+        return `${d.source.data._key}-${d.target.data._key}`
+      })
 
-        },
-        isVertical() {
-            return this.direction === DIRECTION.VERTICL
-        },
-
-        addUniqueKey(rootNode){
-            const queue = [rootNode]
-            while(queue.length !==0) {
-                const node = queue.pop()
-                node._key = uuid()
-
-                if(node.children) {
-                    queue.push (...node.children)
-                }
-            }
-            return rootNode
-        },
-        initTranform() {
-            const containerWidth = this.$refs.container.offsetWidth
-            const containerHeight = this.$refs.container.offsetHeight
-
-            if(this.isVertical()) {
-                this.initTransformX = Math.floor(containerWidth / 2)
-                this.initTransformY = Math.floor(this.config.nodeHeight)
-            }else {
-                this.initTransformX = Math.floor(this.config.nodeWidth)
-                this.initTransformY = Math.floor (containerHeight / 2)
-            }
-            },
-
-            generateLinkPath(d) {
-                const self = this 
-                if(this.LinkStyle === LinkStyle.CURVE) {
-                    const linkPath = this.isVertical()
-                    ? d3.linkVertical() : d3.linkHorizontal()
-                linkPath
-                    .x(function (d) {
-                        return d.x
-                    })
-                    .y(function (d) {
-                        return d.y
-                    })
-                    .source(function (d) {
-                        const sourcePoint = {
-                            x:d.source.x,
-                            y: d.source.y
-                        }
-                        return self.direction === self.DIRECTION.VERTICL
-                        ? sourcePoint
-                        : rotatePoint(sourcePoint)
-                    })
-                    .target(function (d) {
-                        const targetPoint = {
-                            x: d.target.x,
-                            y:d.target.y
-                        }
-                        return self.direction === self.DIRECTION.VERTICL
-                        ? targetPoint
-                        : rotatePoint(targetPoint)
-                    })
-                    return linkPath(d)
-                }
-                if(this.LinkStyle === LinkStyle.STRAIGHT) {
-                    //this link path is source -> secondPoint -> thirdPoint -> target
-
-                    const linkPath = d3.path()
-                    let sourcePoint= {x:d.source.x, y: d.source.y }
-                    let targetPoint = {x:d.target.x , y:d.target.y }
-
-                    if(!this.isVertical()){
-                        sourcePoint = rotatePoint(sourcePoint)
-                        targetPoint = rotatePoint(targetPoint)
-                    }
-
-                    const xOffset = targetPoint.x - sourcePoint.x
-                    const yOffset = targetPoint.y - sourcePoint.y
-
-                    const secondPoint = this.isVertical()
-                        ? {x: sourcePoint.x, y: sourcePoint.y + yOffset/2}
-                        : {x: sourcePoint.x + xOffset/2, y:sourcePoint.y}
-
-                    const thirdPoint = this.isVertical()
-                        ? {x:targetPoint.x, y:sourcePoint.y +yOffset/2}
-                        : {x:sourcePoint.x + xOffset/2, y: targetPoint.y}
-                    linkPath.moveTo(sourcePoint.x, sourcePoint.y)
-                    linkPath.lineTo(secondPoint.x, secondPoint.y)
-                    linkPath.lineTo(thirdPoint.x, thirdPoint.y)
-                    linkPath.lineTo(targetPoint.x, targetPoint.y)
-
-                    return linkPath.toString()
-                }
-            },
-            draw() {
-                const [nodeDataList, linkDataList ] = this.buildTree(this.dataset)
-                this.linkDataList = linkDataList
-                this.svg = this.d3.select(this.$refs.svg)
-
-
-                const self = this
-                const links = this.vg.selectAll('.link').data(linkDataList, (d) => {
-                    return `${d.source.data._key}-${d.target.data._key}`
-                })
-
-                links
-                .enter()
-                .append('path')
-                .style('opacity', 0)
-                .transition()
-                .duration(ANIMATION_DURATION)
-                .ease(d3.easeCubicInOut)
-                .style('opacity', 1)
-                .attr('class', 'link')
-                .attr('d', function (d) {
-                    return self.generateLinkPath(d)
-                })
-                links
+      links
+        .enter()
+        .append('path')
+        .style('opacity', 0)
+        .transition()
+        .duration(ANIMATION_DURATION)
+        .ease(d3.easeCubicInOut)
+        .style('opacity', 1)
+        .attr('class', 'link')
+        .attr('d', function (d) {
+          return self.generateLinkPath(d)
+        })
+      links
         .transition()
         .duration(ANIMATION_DURATION)
         .ease(d3.easeCubicInOut)
@@ -314,6 +311,7 @@ export default {
         .ease(d3.easeCubicInOut)
         .style('opacity', 0)
         .remove()
+
       this.nodeDataList = nodeDataList
     },
     buildTree(rootNode) {
@@ -366,6 +364,7 @@ export default {
         svgElement.style.transform = transformStr
         this.$refs.domContainer.style.transform = transformStr
       }
+
       container.onmouseup = () => {
         startX = 0
         startY = 0
@@ -405,12 +404,8 @@ export default {
       this.draw()
     }
   }
-
 }
 </script>
-
-
-
 
 <style>
 .tree-container .node {
@@ -431,11 +426,13 @@ export default {
   transition: transform 0.8s;
   opacity: 0;
 }
+
 .tree-node-item-enter-active,
 .tree-node-item-leave-active {
   transition-timing-function: ease-in-out;
   transition: all 0.8s;
 }
+
 .tree-container {
   position: relative;
   overflow: hidden;
@@ -443,6 +440,7 @@ export default {
 .tree-container .vue-tree {
   position: relative;
 }
+
 .tree-container > svg, .dom-container {
   width: 100%;
   height: 100%;
@@ -452,9 +450,11 @@ export default {
   overflow: visible;
   transform-origin: 0 50%;
 }
+
 .tree-container .dom-container {
   z-index: 1;
 }
+
 .node-slot {
   position: absolute;
   background-color: transparent;
