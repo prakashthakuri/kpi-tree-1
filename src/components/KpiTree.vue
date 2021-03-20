6d6c96c9108587ffb6ac248a6e01d7015b1a68cc
Prakash Shahi Thakuri <prakashthakuri1994@gmail.com>
2021-03-19 19:23:45 -0500

    Vue KPI Tree v1

--- src/components/KpiTree.vue
+++ src/components/KpiTree.vue
@@ -1,20 +1,71 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <p>
-        This is where the KPI tree should be displayed.
-    </p>
+
+    <div class="tree-container" ref="container">
+      <vue-tree 
+        ref="tree"
+          :style = "{width: treeW + 'px', height: treeH + 'px'}"
+          :dataset = "tree"
+          :config= 'treeConfig'
+          linkStyle = "straight">
+
+          <template v-slot:node="{ node, collapsed, index }">
+                <div class="node-item">
+                    <div style="width:100%">
+                        <div class="node-title">{{node.name}}</div>                        
+                        <div v-show="node.children || collapsed" class="node-toggle" @click="onClickNode(index)">
+                          <v-icon v-if="collapsed" name="chevron-up"></v-icon>                          
+                          <v-icon v-if="!collapsed" name="chevron-down"></v-icon>
+                        </div>
+                    </div>
+                    <div class="node-details">
+                        <div class="node-value node-details-item" :class="node.value.color">
+                            {{node.value.amount}}
+                        </div>
+                        <div class="node-delta node-details-item">
+                            <v-icon :class="node.delta.color" :name="deltaIcon(node.delta.color)" ></v-icon>
+                            {{node.delta.amount}}
+                        </div>                        
+                    </div>
+                </div>
+            </template>
+        </vue-tree>
+
+        <div class="tree-toolbar">
+            <div class="btn-toolbar" @click="zoomIn"><v-icon name="plus"></v-icon></div>
+            <div class="btn-toolbar" @click="zoomReset"><v-icon name="retweet"></v-icon></div>
+            <div class="btn-toolbar" @click="zoomOut"><v-icon name="minus"></v-icon></div>
+        </div>
+    </div>
+
+
+  
   </div>
 </template>
 
 <script>
+
+ import VueTree from './VueTree'
 export default {
   name: 'KpiTree',
   props: {
     msg: String
   },
+  components: {
+    VueTree
+  },
+
   data: function() {
       return {
+          treeW: 1200,
+          treeH:800,
+          treeConfig:{
+            nodeWidth:240,
+            nodeHeight:100,
+            levelHeight: 100
+          },
+          // Static JSON data
           tree: {
             name: "EDITDA", 
             value: {amount: "123", color: "green"}, 
@@ -76,11 +127,50 @@ export default {
             ]
           }
       }
-  }
+  },
+  mounted() {
+    this.resizeHandler()
+  },
+  created() {
+        window.addEventListener("resize", this.resizeHandler);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.resizeHandler);
+  },
+  methods: {
+    resizeHandler () {
+      this.treeW = this.$refs.container.clientWidth
+      this.treeH = this.$refs.container.clientHeight
+    },
+    onClickNode (index) {
+      this.$refs.tree.onClickNode(index)
+    },
+    deltaIcon (icon) {
+      if( icon === "red" ) {
+        return "caret-down"
+      } else if( icon === "green" ) {
+        return "caret-up"
+      } else if( icon === "orange" ) {
+        return "caret-right"
+      }
+      return ""
+    },
+    zoomIn () {
+      this.$refs.tree.zoomIn()
+    },
+    zoomOut () {
+      this.$refs.tree.zoomOut()
+    },
+    zoomReset () {
+      this.$refs.tree.restoreScale()
+    }
+
 }
+}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+ <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -95,5 +185,78 @@ li {
 }
 a {
   color: #42b983;
+}
+
+
+.tree-container {
+  width: 100%;
+  height: 100vh;
+  position: relative;   
+  background: #222; 
+}
+
+.tree-toolbar {
+  display: flex;
+  position: fixed;
+  right: 40px;
+  top: 300px;
+  z-index: 1000;
+}
+
+.btn-toolbar {
+  padding: 12px;
+  color: #F0AB00;
+  text-align: center;
+  cursor: pointer;
+  background-color: #333;
+  border-radius: 8px;
+  margin-right: 12px;
+}
+
+.node-item {
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    color: white;
+    background-color: #333;    
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    text-align: center;
+}
+.node-title {
+    flex-grow: 1;
+    padding: 8px;
+    border-bottom: solid 1px #4c4c4c;
+}
+.node-details {
+    display: flex;    
+    width: 100%;    
+}
+
+.node-value {
+    border-right: solid 1px #4c4c4c;
+}
+
+.node-details-item {
+    width: 100%;
+    padding: 8px;
+}
+.node-toggle {
+  position: absolute;
+  color: #F0AB00;
+  right: 24px;
+  top: 24px;
+  cursor: pointer;
+}
+.red {
+  color: #EE4060;
+}
+.green {
+  color: #06A877;
+}
+.orange {  
+  color: #F0AB00;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+var Icon = require('vue-awesome')
+
+Vue.component('v-icon', Icon)
+
 Vue.config.productionTip = false
 
 new Vue({
